a6ddfc4c003b6db1503de6fbd0c60852ca9f3fe0
Emilio Vázquez <emiliovazquez@Emilios-MacBook-Air.local>
2019-07-05 16:52:31 -0500

    1) Setup the backend with .Net MVC. 2) Sample API test endpoint

---
+++ Backend/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using cynbi.Models;
+
+namespace cynbi.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    [Route("[controller]")]
+    public class TestController: Controller
+    {
+      [HttpGet]
+      public ActionResult Get(){
+        return Ok("asa");
+      }
+    }
+}
